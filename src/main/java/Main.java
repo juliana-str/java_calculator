693171b0830faaa4b7b79460dd439c6372b93aa7
Юлиана Стрельникова <julianka.str@gmail.com>
2024-11-19 16:03:29 +0300

    add main and calculator method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
+    /**
+     *
+     * @param value1 first value
+     * @param value2 second value
+     * @param action action to calculate
+     */
+    public static void calculator(double value1, double value2, String action) {
+        double result = 0;
+        switch (action) {
+            case "+":
+                result = value1 + value2;
+                break;
+            case "-":
+                result = value1 - value2;
+                break;
+            case "*":
+                result = value1 * value2;
+                break;
+            case "/":
+                result = value1 / value2;
+        }
+        System.out.printf(String.format("Результат вычисления %.4f", result));
+    }
 
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Введите первое число.");
+        Scanner scanner = new Scanner(System.in);
+        double value1 = scanner.nextDouble();
+        System.out.println("Введите второе число.");
+        double value2 = scanner.nextDouble();
+        System.out.println("Пожалуйста введите необходимое действие.");
+        String action = scanner.next();
+        scanner.close();
+        calculator(value1, value2, action);
+    }
 }
