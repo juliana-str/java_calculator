91f0d21cb52878006388d431ddd3a762d04f5056
Юлиана Стрельникова <julianka.str@gmail.com>
2024-11-20 10:51:56 +0300

    add findMaxElement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     /**
-     *
      * @param value1 first value
      * @param value2 second value
      * @param action action to calculate only +, -, *, /
@@ -26,18 +28,45 @@ public class Main {
     }
 
     /**
-     * @author Strelnikova Juliana
+     * @param words list of entered words
+     */
+    public static void findMaxElement(List<String> words) {
+        String maxLenthWord = "";
+        for (String word : words) {
+            if (word.length() > maxLenthWord.length()) {
+                maxLenthWord = word;
+            }
+        }
+        System.out.println(maxLenthWord);
+    }
+
+    /**
      * @param args
+     * @author Strelnikova Juliana
      */
     public static void main(String[] args) {
-        System.out.println("Введите первое число.");
         Scanner scanner = new Scanner(System.in);
-        double value1 = scanner.nextDouble();
-        System.out.println("Введите второе число.");
-        double value2 = scanner.nextDouble();
-        System.out.println("Пожалуйста введите необходимое действие.");
-        String action = scanner.next();
+        System.out.println("Введите номер задачи  (1 - calculator, 2 - string array).");
+        int taskNumber = scanner.nextInt();
+        if (taskNumber == 1) {
+            System.out.println("Введите первое число.");
+            double value1 = scanner.nextDouble();
+            System.out.println("Введите второе число.");
+            double value2 = scanner.nextDouble();
+            System.out.println("Пожалуйста введите необходимое действие.");
+            String action = scanner.next();
+            calculator(value1, value2, action);
+        } else {
+            System.out.println("Введите число - размер массива слов.");
+            int arrayLenth = scanner.nextInt();
+            List<String> words = new ArrayList<>();
+            for (int i = 1; i <= arrayLenth; i++) {
+                System.out.println("Введите " + i + " слово.");
+                String word = scanner.next();
+                words.add(word);
+            }
+            findMaxElement(words);
+        }
         scanner.close();
-        calculator(value1, value2, action);
     }
 }
